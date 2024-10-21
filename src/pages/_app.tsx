7c681b9550ab315f0eadb043dd1d6fd89b86c165
gargr01099 <rahulgarg.eminence@gmail.com>
2024-10-21 10:48:12 +0530

    fix

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -7,12 +7,13 @@ import '../styles/global.css';
 import { AppProps } from "next/app";
 import { PopupProvider } from "../context/PopupContext";
 import Popup from "../components/Popup";
-
 const MyApp: React.FC<AppProps> = ({ Component, pageProps }) => {
   return (
     <PopupProvider>
       <Component {...pageProps} />
-      <Popup />
+      <Popup message={null} onClose={function (): void {
+        throw new Error('Function not implemented.');
+      } } />
     </PopupProvider>
   );
 };
